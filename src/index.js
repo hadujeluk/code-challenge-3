58f6cab35d48a3e4cba327397c55446e501ca44a
judahluke <judahluke58@gmail.com>
2024-04-11 01:17:50 +0300

    initial  commit

---
+++ src/index.js
@@ -0,0 +1,116 @@
+let allMovies = [];
+let currentMovie = null;
+
+function getMovies() {
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      
+      fetch("http://localhost:3000/films", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            //console.log(result);
+            allMovies = result;
+            ListMovies(result);
+        })
+        .catch((error) => console.error(error));
+}
+
+function ListMovies(movies) {
+    const movieList = document.getElementById("films");
+    let html = "";
+
+    for (let i = 0; i < movies.length; i++) {
+      let movie = movies[i];
+      //console.log(movie);
+      //console.log(typeof movie);
+      html = html + `<li class="film item" onclick="clickedMovie(${i})">${movie.title}</li>`;
+    }
+
+    movieList.innerHTML = html;
+}
+getMovies();
+
+
+function clickedMovie(i) {
+    let poster = document.getElementById("poster");
+    let clickedMovie = allMovies[i];
+    poster.src = clickedMovie.poster;
+    poster.alt = clickedMovie.title;
+    movieInfo(clickedMovie.id);
+}
+
+function movieInfo(id) {
+    const title = document.getElementById("title");
+    const runtime = document.getElementById("runtime");
+    const info = document.getElementById("film-info");
+    const showtime = document.getElementById("showtime");
+    const ticket = document.getElementById("ticket-num");
+    const btn = document.getElementById("buy-ticket");
+    
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+      
+      fetch(`http://localhost:3000/films/${id}`, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            title.innerText = result.title;
+            runtime.innerText = `${result.runtime} minutes`;
+            info.innerText = result.description;
+            showtime.innerText = result.showtime;
+            ticket.innerText = `remaining tickets ${
+                result.capacity - result.tickets_sold
+            }`;
+            console.log(result);
+            currentMovie = result;
+
+            btn.addEventListener("click", function() {
+              console.log("Buy movie");
+              let result = currentMovie;
+              let remainingTickets = result.capacity - result.tickets_sold;
+            
+              if(remainingTickets > 0) {
+                makeASale(result);
+              }else {
+                  ticket.innerText = "!!! Sold OUT";
+              }
+          });
+          })
+        .catch((error) => console.error(error));
+}
+
+
+function makeASale(movie) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    title: movie.title,
+    runtime: movie.runtime,
+    capacity: movie.capacity,
+    showtime: movie.showtime,
+    tickets_sold: movie.tickets_sold + 1,
+    description: movie.description,
+    poster: movie.poster,
+});
+
+
+const requestOptions = {
+  method: "PUT",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(`http://localhost:3000/films/${movie.id}`, requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    //console.log(result);
+    movieInfo(movie.id);
+})
+  .catch((error) => console.error(error));
+}
